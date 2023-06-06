8fb5d7c59f385908a5fbe87bfc21b812a6e218b5
bamansour1978 <karatman@gmail.com>
2023-06-07 00:24:52 +0100

    Fix 99 problem

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for Num in range(0, 100):
-    print("{:02}".format(Num), end=", ")
+    if Num == 99:
+        print("{}".format(Num))
+    else:
+        print("{:02}".format(Num), end=", ")
