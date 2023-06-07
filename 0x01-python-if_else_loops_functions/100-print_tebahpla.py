e62a79e1d3cb6466beb2fb164e644fdb6f6a0668
bamansour1978 <karatman@gmail.com>
2023-06-07 02:56:37 +0100

    FIx a problem

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 j = 0
-for s in range(ord('z'), ord('a') -1, -1):
+for s in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(s - j)), end="")
     j = 32 if j == 0 else 0
