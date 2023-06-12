34775c5a7c9e1e436345e8480527259a952a735e
bamansour1978 <karatman@gmail.com>
2023-06-12 18:04:12 +0100

    Repalce

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_str = ""
+    new_string = ""
     for char in my_string:
-        if char != 'c' and char != 'C':
-            new_str += char
-    return new_str
+        if char != 'C' and char != 'c':
+            new_string += char
+    return new_string
