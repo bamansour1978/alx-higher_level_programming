10f472570977d347a263fc99bca7b32186ddca68
bamansour1978 <karatman@gmail.com>
2023-06-15 00:01:12 +0100

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    a = new_list
+    b = len(new_list)
+    return [a[i] if a[i] != search else replace for i in range(b)] 
