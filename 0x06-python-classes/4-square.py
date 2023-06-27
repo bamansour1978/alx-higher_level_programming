cb0ff76ec4a8cdbc14046d7f46c2d92c0e61fc1f
bamansour1978 <karatman@gmail.com>
2023-06-28 00:58:16 +0100

    Fix Error : can't multiply sequence by non-int of type 'str'

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
 # 4-square.py
 # BAMANSOUR Abdennebi
-"""a class Square that defines a square by: (based on 3-square.py)"""
+"""A class Square that defines a square."""
 
 
 class Square:
     """A square"""
 
     def __init__(self, size=0):
-        """Init a new Square.
+        """Initialize a new Square.
 
         Args:
             size (int): The new square's size.
         """
         self.size = size
 
-        @property
-        def size(self):
-            """ size of squre"""
-            return (self._Square__size)
+    @property
+    def size(self):
+        """Size of the square"""
+        return self._Square__size
 
-        @size.setter
-        def size(self, size):
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self._Square__size = size
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
 
     def area(self):
-        """ function return area of the square"""
-        return (self.size * self.size)
+        """Return the area of the square"""
+        return self.size * self.size
