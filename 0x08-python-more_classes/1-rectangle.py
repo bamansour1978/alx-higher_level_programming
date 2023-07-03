922f09ae3935f62c29bd497ca2368dac8e8d8150
bamansour1978 <karatman@gmail.com>
2023-07-03 21:30:53 +0100

    Fix a documentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 # 1-rectangle.py
-
+""" A rectangle class"""
 
 class Rectangle:
-
+    """ A rectangle representation"""
+    
     def __init__(self, width=0, height=0):
-
+        """ Initialize a rectangle
+        args:
+            - `width`: the length of one side (int > 0).
+            - `height`: the other side's length (int > 0).
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ Get the width of a rectangle"""
         return self.__width
 
     @width.setter
@@ -23,6 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the heigth of a rectangle."""
         return self.__height
 
     @height.setter
