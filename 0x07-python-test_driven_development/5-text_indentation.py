d543dccbacb7300104a7e9915ba4dfb3dfbf6022
bamansour1978 <karatman@gmail.com>
2023-07-05 02:47:08 +0100

    Task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/
+# 5-text_indentation.py
+""" A text_indentation """
 
-def text_identation(text):
 
+def text_indentation(text):
+    """
+    This function takes in one argument, 
+    which should be the inputted paragraph of text and returns it
+
+    args:
+        - `text`: The given paragraph 
+    raise:
+        if text isn't str
+    
+    """
     if type(text) is not str:
         raise TypeError('text must be a string')
     re_t = text.replace('.', '.\n\n')
