8c8d6cd42f0b85811e52079aec18dbdc8884d048
bamansour1978 <karatman@gmail.com>
2023-07-11 01:27:48 +0100

    Fix Exeption raise

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
     """Represent base_geometry."""
     def area(self):
         """case : not implemented"""
-        raiseException("area() is not implemented")
+        raise Exception("area() is not implemented")
