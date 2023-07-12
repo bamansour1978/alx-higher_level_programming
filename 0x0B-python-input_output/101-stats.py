c6581a8261a1c89498b750170794212087f9a25c
bamansour1978 <karatman@gmail.com>
2023-07-13 00:49:28 +0100

    14. Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# 101-stats.py
+"""A script that reads stdin line by line and computes metrics"""
+
+
+def print_stats(size, status_codes):
+    """ Return print accumulated metrics.
+
+    Args:
+        size : int
+        status_codes : dict
+    """
+    print("File size: {}".format(size))
+    for k in sorted(status_codes):
+        print("{}: {}".format(k, status_codes[k]))
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    counter = 0
+
+    try:
+        for l in sys.stdin:
+            if counter == 10:
+                print_stats(size, status_codes)
+                counter = 1
+            else:
+                counter += 1
+
+            l = l.split()
+
+            try:
+                size += int(l[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if l[-2] in valid_codes:
+                    if status_codes.get(l[-2], -1) == -1:
+                        status_codes[l[-2]] = 1
+                    else:
+                        status_codes[l[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
