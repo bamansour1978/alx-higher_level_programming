86ea64b44eacbe09cd3d2162d29e1d87c94f5b7a
bamansour1978 <karatman@gmail.com>
2023-07-13 00:22:59 +0100

    add str function

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,12 +17,5 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
-
-class Square(Rectangle):
-    def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(self.__size, self.__size)
-
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
