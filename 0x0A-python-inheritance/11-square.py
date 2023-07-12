7ee5f7a5c1fdb1522287f578f6d07dfcea2e6ea8
bamansour1978 <karatman@gmail.com>
2023-07-12 13:19:08 +0100

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 11-square.py
+""" A Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A square's repr."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size : new square's size (int).
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
