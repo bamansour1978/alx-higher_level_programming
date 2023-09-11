fe6754555ea873a1d41159c40b3c101756a90b5f
bamansour1978 <karatman@gmail.com>
2023-09-12 00:33:29 +0100

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
+
 const { argv } = require('process');
-const a = parseInt(argv[2], 10);
-function factorial (a) {
-    if (a <= 1) return 1;
-    return a * factorial(a - 1);
+if (argv.length < 3) {
+  console.error("Veuillez fournir un nombre en argument.");
+  process.exit(1); // Quitter le script avec un code d'erreur
 }
+
+const a = parseInt(argv[2], 10);
+
+function factorial(a) {
+  if (a <= 1) return 1;
+  return a * factorial(a - 1);
+}
+
 if (isNaN(a)) console.log(1);
 else console.log(factorial(a));
