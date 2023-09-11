a6e3b3e58a809631ff98f4f9eed7f305838ec9cf
bamansour1978 <karatman@gmail.com>
2023-09-11 23:28:14 +0100

    FIx error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arrayStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < arrayStr.length; i++) {
-  console.log(arrayStr[i]);
+const arrStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let j = 0; j < arrStr.length; j++) {
+  console.log(arrStr[j]);
 }
