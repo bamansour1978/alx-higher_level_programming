57edcb425cda1664ae8a7645e646265321242c54
bamansour1978 <karatman@gmail.com>
2023-09-17 18:23:48 +0100

    chmod add

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 
-"""states models
+"""Lists all states from the database hbtn_0e_0_usa.
 """
 if __name__ == "__main__":
-    import MySQLdb
     import sys
+    import MySQLdb
 
     db_host = "localhost"
-    db_user = sys.argv[1]  # "your_username"
-    db_password = sys.argv[2]  # "your_password"
-    db_name = sys.argv[3]  # "your_database_name"
+    db_user = sys.argv[1]  # "username"
+    db_password = sys.argv[2]  # "password"
+    db_name = sys.argv[3]  # "database_name"
     port = 3306
 
     db = MySQLdb.connect(
         host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
     )
-    cursor = db.cursor()
+    c = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = c.fetchall()
 
-    for row in rows:
-        print(row)
+    for r in rows:
+        print(r)
 
-    cursor.close()
+    c.close()
     db.close()
