adc918d6d310f0f1ad551a8ffdca7fc96b1eeb57
bamansour1978 <karatman@gmail.com>
2023-09-17 18:57:42 +0100

    Fix error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 
-"""states models
+"""1-filter_states.py
 """
+
 if __name__ == "__main__":
-    import MySQLdb
     import sys
+    import MySQLdb
 
     db_host = "localhost"
-    db_user = sys.argv[1]  # "your_username"
-    db_password = sys.argv[2]  # "your_password"
-    db_name = sys.argv[3]  # "your_database_name"
+    db_user = sys.argv[1]  # "username"
+    db_password = sys.argv[2]  # "password"
+    db_name = sys.argv[3]  # "database_name"
     port = 3306
 
     db = MySQLdb.connect(
         host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
     )
-    cursor = db.cursor()
+    c = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name \
+    c.execute("SELECT * FROM states WHERE name \
 LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
 
-    for row in rows:
-        print(row)
+    for r in rows:
+        print(r)
 
-    cursor.close()
+    c.close()
     db.close()
