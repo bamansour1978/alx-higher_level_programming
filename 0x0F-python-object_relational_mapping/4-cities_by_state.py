a5133d9857865af9874fd63f596d8f8cff463bd5
bamansour1978 <karatman@gmail.com>
2023-09-17 20:33:53 +0100

    Add chmod

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,19 +6,19 @@ if __name__ == "__main__":
     import sys
 
     db_host = "localhost"
-    db_user = sys.argv[1]  # "username"
-    db_password = sys.argv[2]  # "password"
-    db_name = sys.argv[3]  # "database_name"
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
     port = 3306
-    state_name = sys.argv[4]  # "database_name"
-    query = "SELECT * FROM states WHERE name LIKE BINARY\
- '{}' ORDER BY id ASC".format(state_name)
+
     db = MySQLdb.connect(
         host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
     )
     c = db.cursor()
 
-    c.execute(query)
+    c.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON states.id = cities.state_id \
+    ORDER BY cities.id")
     rows = c.fetchall()
 
     for r in rows:
