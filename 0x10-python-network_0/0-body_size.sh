78b210018cdebd45c668cb668c8627bca0e160a5
bamansour1978 <karatman@gmail.com>
2023-09-28 22:31:11 +0100

    Task 0 correct

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-#+ The size must be displayed in bytes
-#+ You have to use curl
+# A Bash script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response in bytes
+# You have to use curl
 
-curl -s "$1" | wc -c
+# Check if the user provided a URL as an argument
+curl -sw '%{size_download}\n' -o /dev/null "$1"
