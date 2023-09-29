6b22df81c1c03726880b1c3a60892193d2352315
bamansour1978 <karatman@gmail.com>
2023-09-29 23:51:39 +0100

    Update 10 file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""./9-script.py <search string>
+"""./script.py <GitHub username> <GitHub password>
 """
+
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    url = "https://swapi.co/api/people"
-    parms = {"search": sys.argv[1]}
-    reslts = requests.get(url, params=parms).json()
-
-    print("Number of results: {}".format(reslts.get("count")))
-    [print(r.get("name")) for r in reslts.get("results")]
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
