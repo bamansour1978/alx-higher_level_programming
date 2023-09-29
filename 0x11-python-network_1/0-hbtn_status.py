64b040599b71d95af30ea2f0ffe0aa7b7f9f0f73
bamansour1978 <karatman@gmail.com>
2023-09-29 21:58:09 +0100

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that fetches https://intranet.hbtn.io/status."""
+import urllib.request
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as res_ponse:
+        bdy = res_ponse.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(bdy)))
+        print("\t- content: {}".format(bdy))
+        print("\t- utf8 content: {}".format(bdy.decode("utf-8")))
