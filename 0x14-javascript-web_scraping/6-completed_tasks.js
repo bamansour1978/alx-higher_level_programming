aab8ebe06205df703824b59456374f0d92343758
bamansour1978 <karatman@gmail.com>
2023-10-24 23:04:29 +0100

    upadate

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
     const todos = JSON.parse(body);
     let completed = {};
+
     todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+      if (todo.completed) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId] += 1;
+        }
       }
     });
-    console.log(completed);
+
+    for (const userId in completed) {
+      console.log(`User ${userId}: ${completed[userId]} completed tasks`);
+    }
   } else {
-    console.error("error:", error);
+    console.error("Error:", error);
   }
 });
