98e80cf419b75eebce1cf151219a197a76fa6018
bamansour1978 <karatman@gmail.com>
2023-10-24 21:52:01 +0100

    Update

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-
-//import request
 const request = require('request');
-
-//const the url
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-
-// use req & log title
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
