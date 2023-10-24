7295f4769fa58c8c60a9c3f3774409fc35e37185
bamansour1978 <karatman@gmail.com>
2023-10-24 21:25:57 +0100

    3. Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+//import request
+const request = require('request');
+
+//const the url
+let url = 'https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2];
+
+// use req & log title
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
